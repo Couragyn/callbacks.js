f36010f8944c2fe7252cebced57924e5bc57bfb1
Couragyn Chretien <couragyn@gmail.com>
2017-03-09 20:02:49 +0000

    refactored to use forEach on findWaldo

--- waldo.js
+++ waldo.js
@@ -1,8 +1,8 @@
 // The second argument/parameter is expected to be a function
 function findWaldo(arr, found) {
-  for (var i = 0; i < arr.length; i++) {
-    if (arr[i] === "Waldo") {
-      found(i);   // execute callback
+  arr.forEach(found(element) {
+    if (arr[element] === "Waldo") {
+      found(element);   // execute callback
     }
   }
 }
@@ -12,5 +12,3 @@ function actionWhenFound(index) {
 }
 
 findWaldo(["Alice", "Bob", "Waldo", "Winston"], actionWhenFound);
-
-// Modify the callback function in the previous example so that logs the index of the array where Waldo is found, ie. "Found Waldo at index 2!". (You will need to modify actionWhenFound to receive the index.)
